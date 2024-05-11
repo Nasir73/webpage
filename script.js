117a2178335382e230f19c32651ea414a48ed74b
Nasir73 <155837894+Nasir73@users.noreply.github.com>
2024-05-11 21:03:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Function to redirect to Amazon product page
+function redirectToAmazon(amazonUrl) {
+    window.open(amazonUrl, '_blank');
+}
+
+// Function to redirect to Flipkart product page
+function redirectToFlipkart(flipkartUrl) {
+    window.open(flipkartUrl, '_blank');
+}
